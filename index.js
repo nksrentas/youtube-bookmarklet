2b39783f7793d7b8ee004768130992365fde561a
satner <cse2343@gmail.com>
2019-04-20 21:14:26 +0300

    extract all channel names

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ javascript: (function () {
     let searchText = document.createTextNode('WELCOME TO KAPPA R E A L I T Y');
     let searchBar = document.createElement('input');
     let renderedSubcriptionChannelsDIV = subscriptionMenu.querySelectorAll('div.style-scope.ytd-guide-section-renderer ytd-guide-entry-renderer');
-    Array.prototype.map.call(renderedSubcriptionChannelsDIV, function (data) {
+    Array.prototype.map.call(renderedSubcriptionChannelsDIV, function (data, index) {
         channelName = data.querySelector('a').title;
-        console.log(": channelName", channelName);
+        console.log("channelName: ", channelName);
+        if (index == 7) {
+            data.click();
+            let expandListDIV = document.querySelectorAll('div#expanded div#expandable-items .style-scope.ytd-guide-collapsible-entry-renderer');
+            Array.prototype.map.call(expandListDIV, function (data2, index) {
+                expandedChannelName = data2.querySelector('a').title;
+                console.log("channelName: ", expandedChannelName, index);
+            });
+        }
     });
     searchBar.placeholder = "Search for channel...";
     searchBar.style.width = "100%";
@@ -22,3 +30,5 @@ javascript: (function () {
     searchDIV.style.color = 'red';
     subscriptionMenu.insertBefore(searchDIV, subscriptionMenu.childNodes[2]);
 })();
+
+
