2522f227b016c22f6b6fc4dd62938cc9730f0f99
satner <cse2343@gmail.com>
2019-04-20 21:32:01 +0300

    main functionality completed!

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ javascript: (function () {
     let searchText = document.createTextNode('WELCOME TO KAPPA R E A L I T Y');
     let searchBar = document.createElement('input');
     let renderedSubcriptionChannelsDIV = subscriptionMenu.querySelectorAll('div.style-scope.ytd-guide-section-renderer ytd-guide-entry-renderer');
+    let allChannels = [];
     Array.prototype.map.call(renderedSubcriptionChannelsDIV, function (data, index) {
-        channelName = data.querySelector('a').title;
-        console.log("channelName: ", channelName);
+        if (index <= 6) {
+            allChannels.push(data);
+        }
         if (index == 7) {
             data.click();
             let expandListDIV = document.querySelectorAll('div#expanded div#expandable-items .style-scope.ytd-guide-collapsible-entry-renderer');
             Array.prototype.map.call(expandListDIV, function (data2, index) {
-                expandedChannelName = data2.querySelector('a').title;
-                console.log("channelName: ", expandedChannelName, index);
+                if (index !== expandListDIV.length - 1) {
+                    allChannels.push(data2);
+                }
             });
         }
     });
@@ -22,8 +25,14 @@ javascript: (function () {
     searchBar.style.padding = "5px";
     searchBar.addEventListener('keyup', (e) => {
         let filterValue = e.target.value.toUpperCase();
-        console.log(filterValue);
-
+        for (let i = 0; i < allChannels.length; i++) {
+            a = allChannels[i].querySelector('a').title.toUpperCase();
+            if (a.indexOf(filterValue) > -1) {
+                allChannels[i].style.display = "";
+            } else {
+                allChannels[i].style.display = "none";
+            }
+        }
     });
     searchDIV.appendChild(searchText);
     searchDIV.appendChild(searchBar);
